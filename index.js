71de5a3ecae081e7e4f66facd51b21379712235c
Fernando Stadler <fernandostadler2@gmail.com>
2023-12-04 04:21:28 -0300

    padronização de mensagens de erro

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const URL_API = 'http://localhost:3000';
 app.use(express.json());
 app.use(cors({credentials: true, origin: URL_API}));
 
-app.use((req, res) => {
-    res.status(404).json({ message: 'A página não foi localizada ou não existe' });
-  });
-
 app.use('/users', userRoutes)
+
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'A página não foi localizada ou não existe!' });
+  });
+  
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
