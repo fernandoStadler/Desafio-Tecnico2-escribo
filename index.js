037c6d46a38f7c9ef5dcd7f631d54968b1c784a3
Fernando Stadler <fernandostadler2@gmail.com>
2023-12-04 02:21:23 -0300

    hospdeagem vercel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./routes/userRoutes.js');
 const cors = require('cors');
 
 const app = express();
-const URL_API = 'https://desafio-tecnico2-escribo.vercel.app/';
+const URL_API = 'https://desafio-tecnico2-escribo-l5dlpichc-fernandostadler.vercel.app';
 
 app.use(express.json());
 app.use(cors({credentials: true, origin: URL_API}));
