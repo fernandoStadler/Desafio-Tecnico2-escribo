212ae29d6ddce28172e8877733987b29094f1380
Fernando Stadler <fernandostadler2@gmail.com>
2023-12-04 02:07:09 -0300

    Padronização erro 404

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const URL_API = 'http://localhost:3000';
 app.use(express.json());
 app.use(cors({credentials: true, origin: URL_API}));
 
+app.use((req, res) => {
+    res.status(404).json({ message: 'A página não foi localizada ou não existe' });
+  });
+
 app.use('/users', userRoutes)
 
 app.listen(3000, () => {
